125c5e86b8917c3e72198cdc7fc1ca29b97d1087
ORC-1 <Rolandazim@gmail.com>
2018-08-04 23:30:45 +0100

    Form error, access restriction fix

--- metailbook/urls.py
+++ metailbook/urls.py
@@ -1,18 +1,4 @@
-"""metailbook URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import  include
 from django.conf.urls import url
 from django.contrib import admin
@@ -27,7 +13,7 @@ urlpatterns = [
     # url('(?P<pk>\d+)$', mviews.indexDetailView.as_view(), name='index'),
     url(r'^$', mviews.SignIn, name='SignIn'),
     url(r'^signup/$', mviews.signup, name='signup'),
-    url(r'^home/$, mviews.homepage, name='homepage'),
+    url(r'^home/$', mviews.homepage, name='homepage'),
     url(r'^account/$', mviews.account_profile_form, name='account'),
     url(r'^customer/$', mviews.customer_form, name='customer'),
     url(r'^blouse/$', mviews.blouse_form, name='blouse'),
